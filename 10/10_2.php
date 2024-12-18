88b49985b35559ec365fe1d8ea968c0f970f9dca
Oliver <807100+Friedfisch@users.noreply.github.com>
2024-12-18 12:35:12 +0100

    10.2

--- 10/10_2.php
+++ 10/10_2.php
@@ -5,7 +5,7 @@ require_once __DIR__ . '/10_2_graph.php';
 $o = Graph::test1();
 $result = $o->run();
 print_r("Result 1 is $result\n");
-exit;
+
 $o = Graph::calc();
 $result = $o->run();
 print_r("Result is $result\n");
